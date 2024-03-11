b26521b8b6f700b5c8ab85ed539bf2c4b1c3e673
sachitha08 <160518752+sachitha08@users.noreply.github.com>
2024-03-11 21:34:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function startCountdown() {
+  var datetime = document.getElementById("datetime").value;
+  var countDownDate = new Date(datetime).getTime();
+
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
